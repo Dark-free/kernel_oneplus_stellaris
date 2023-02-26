King-Kernel/KingKernel-floral

zram: Expose compression algorithms to userspace
This makes it easier to debug/edit zram compression algorithms for the user.
The user can change from the default algorithm to something else

File in sysfs: "/sys/module/zram/parameters/default_compressor"

Signed-off-by: kingbri <bdashore3@gmail.com>

drivers/block/zram/zram_drv.c
@@ -33,6 +33,7 @@
 #include <linux/sysfs.h>
 #include <linux/debugfs.h>
 #include <linux/cpuhotplug.h>
+#include <linux/moduleparam.h>
 
 #include "zram_drv.h"
 
@@ -41,7 +42,9 @@ static DEFINE_IDR(zram_index_idr);
 static DEFINE_MUTEX(zram_index_mutex);
 
 static int zram_major;
-static const char *default_compressor = CONFIG_ZRAM_DEFAULT_COMP_ALGORITHM;
+static char *default_compressor = CONFIG_ZRAM_DEFAULT_COMP_ALGORITHM;
+
+module_param(default_compressor, charp, 0644);
 
 /* Module params (documentation at end) */
 static unsigned int num_devices = 1;

Dark-free/kernel_oneplus_stellaris

Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

drivers/block/zram/zram_drv.c
@@ -34,6 +34,7 @@
 #include <linux/sysfs.h>
 #include <linux/debugfs.h>
 #include <linux/cpuhotplug.h>
+#include <linux/moduleparam.h>
 
 #include "zram_drv.h"
 #include "zram_drv_internal.h"
@@ -46,7 +47,9 @@ static DEFINE_IDR(zram_index_idr);
 static DEFINE_MUTEX(zram_index_mutex);
 
 static int zram_major;
-static const char *default_compressor = CONFIG_ZRAM_DEF_COMP;
+static char *default_compressor = CONFIG_ZRAM_DEF_COMP;
+
+module_param(default_compressor, charp, 0644);
 
 /* Module params (documentation at end) */
 static unsigned int num_devices = 1;